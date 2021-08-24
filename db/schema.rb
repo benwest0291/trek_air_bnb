a7c1d5826d4d89b063695b8782a40c20e3a94d46
Megan Luengo <87246548+phenelagayle@users.noreply.github.com>
2021-08-24 11:21:08 +0100

    updated user model and container for signup

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_23_151843) do
+ActiveRecord::Schema.define(version: 2021_08_23_153539) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
