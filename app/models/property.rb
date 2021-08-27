144c5a706a15ae906e7968fa7bf91caff4ffc245
chiara ferrari <chi.ferr1188@gmail.com>
2021-08-27 12:37:06 +0100

    search

--- app/models/property.rb
+++ app/models/property.rb
@@ -6,12 +6,12 @@ class Property < ApplicationRecord
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
 
-  # include PgSearch::Model
-  # pg_search_scope :search_by_name_and_address,
-  #  against: [ :name, :address ],
-  #  using: {
-  #    tsearch: { prefix: true }
-  #   }
+  include PgSearch::Model
+  pg_search_scope :search_by_name_and_address,
+   against: [ :name, :address ],
+   using: {
+     tsearch: { prefix: true }
+    }
 
   validates :postcode, :price, :name, presence: true
 end
